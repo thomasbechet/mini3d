9f61aca50b94c086d4fb2886d2899d18cfe8b037
Thomas <thomas.bechet.pro@gmail.com>
2023-03-05 17:57:41 +0100

    fix pixel mip-mapping

--- mini3d_wgpu/src/renderer.rs
+++ mini3d_wgpu/src/renderer.rs
@@ -117,7 +117,7 @@ impl WGPURenderer {
             address_mode_v: wgpu::AddressMode::ClampToEdge,
             address_mode_w: wgpu::AddressMode::ClampToEdge,
             mag_filter: wgpu::FilterMode::Nearest,
-            min_filter: wgpu::FilterMode::Linear,
+            min_filter: wgpu::FilterMode::Nearest,
             mipmap_filter: wgpu::FilterMode::Nearest,
             ..Default::default()
         });
