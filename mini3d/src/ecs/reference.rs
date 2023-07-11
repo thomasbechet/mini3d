848b50a44344ebaf13b153012d4522f7fe7ddd92
Thomas BECHET <thomas.bechet.pro@gmail.com>
2023-07-11 07:40:39 +0200

    wip component reference

--- mini3d/src/ecs/reference.rs
+++ mini3d/src/ecs/reference.rs
@@ -1,14 +1,100 @@
-use std::ops::{Deref, DerefMut};
+use glam::{IVec2, IVec3, IVec4, Mat4, Quat, Vec2, Vec3, Vec4};
 
-use crate::{
-    script::reflection::{ReadProperty, ReadWriteProperty},
-    uid::UID,
-};
+use crate::uid::UID;
 
 use super::entity::Entity;
+use crate::ecs::view::AnyComponentViewRef;
+use crate::ecs::view::AnyComponentViewRefInner;
+use crate::script::reflection::PropertyId;
 
-pub struct ComponentRef {
+pub struct AnyComponentRef<'a> {
+    _lifetime: core::marker::PhantomData<&'a ()>,
     entity: Entity,
     index: usize,
-    ty: UID,
+}
+
+macro_rules! impl_read_property {
+    ($type:ty, $read:ident) => {
+        pub fn $read(&self, id: PropertyId, view: &AnyComponentViewRef) -> Option<$type> {
+            match &view.0 {
+                AnyComponentViewRefInner::Static {
+                    components,
+                    entities,
+                    indices: _,
+                } => {
+                    if entities[self.index] == self.entity {
+                        components.$read(self.index, id)
+                    } else {
+                        None
+                    }
+                }
+                AnyComponentViewRefInner::Dynamic {} => None,
+                AnyComponentViewRefInner::None => None,
+            }
+        }
+    };
+}
+
+impl<'a> AnyComponentRef<'a> {
+    impl_read_property!(bool, read_bool);
+    impl_read_property!(u8, read_u8);
+    impl_read_property!(i32, read_i32);
+    impl_read_property!(u32, read_u32);
+    impl_read_property!(f32, read_f32);
+    impl_read_property!(f64, read_f64);
+    impl_read_property!(Vec2, read_vec2);
+    impl_read_property!(IVec2, read_ivec2);
+    impl_read_property!(Vec3, read_vec3);
+    impl_read_property!(IVec3, read_ivec3);
+    impl_read_property!(Vec4, read_vec4);
+    impl_read_property!(IVec4, read_ivec4);
+    impl_read_property!(Mat4, read_mat4);
+    impl_read_property!(Quat, read_quat);
+    impl_read_property!(Entity, read_entity);
+    impl_read_property!(UID, read_uid);
+}
+
+pub struct AnyComponentMut<'a> {
+    _lifetime: core::marker::PhantomData<&'a ()>,
+    entity: Entity,
+    index: usize,
+}
+
+macro_rules! impl_read_write_property {
+    ($type:ty, $read:ident, $write:ident) => {
+        pub fn $read(&self, id: PropertyId, view: &AnyComponentViewMut) -> Option<$type> {
+            match &self.0 {
+                AnyComponentViewMutInner::Static {
+                    components,
+                    entities,
+                    indices,
+                } => {
+                    if entities[self.index] == self.entity {
+                        components.$read(self.index, id)
+                    } else {
+                        None
+                    }
+                }
+                AnyComponentViewMutInner::Dynamic {} => None,
+                AnyComponentViewMutInner::None => None,
+            }
+        }
+        pub fn $write(&self, id: PropertyId, value: $type, view: &mut AnyComponentViewMut) {
+            match &self.0 {
+                AnyComponentViewMutInner::Static {
+                    components,
+                    entities,
+                    indices,
+                } => {
+                    if entities[self.index] == self.entity {
+                        components.$write(self.index, id)
+                    } else {
+                        None
+                    }
+                }
+                AnyComponentViewMutInner::Dynamic {} => None,
+                AnyComponentViewMutInner::None => None,
+            }
+        }
+    };
 }
